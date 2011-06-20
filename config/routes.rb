d33d4e4fde725725c38c7c0d33d5df116599e1c0
Andrew Ferris <aferris@alumni.tufts.edu>
2011-06-20 15:50:02 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -59,5 +61,5 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-
+  match '/signup', :to => 'users#new'
 end
